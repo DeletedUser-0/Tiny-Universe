0b0183ce2b27e6cd99ee98af690b10ad691e8056
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2023-06-04 12:16:14 +0100

    Create disable_inspect_element.js

---
+++ disable_inspect_element.js
@@ -0,0 +1,17 @@
+// Disable Inspect Element
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
